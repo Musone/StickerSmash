3740eb39a11aec25ff900e4d6ac677cf9678e51e
Jakob <yakobkh@gmail.com>
2023-02-25 02:06:59 -0800

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (result.canceled) {
+      alert("Image selection canceled.");
+      return;
+    }
+
+    setSelectedImage(result.assets[0].uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={require("./assets/images/background-image.png")}
+          selectedImage={selectedImage ?? null}
+        />
+        <Button label="Select image" theme="primary" onPress={pickImageAsync} />
+        <Button
+          label="Use this image"
+          onPress={() => alert("You pressed the button. Good job.")}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//     paddingTop: 10,
+//   },
+//   image: {
+//     width: 100,
+//     height: 200,
+//     borderRadius: 20,
+//   }
+// });
---
+++ components/ImageViewer.tsx
@@ -0,0 +1,28 @@
+import { dirname } from "path";
+import { FunctionComponent } from "react";
+import { StyleSheet, Image, View } from "react-native";
+
+interface ImageViewerProps {
+  placeholderImageSource: any; // require(<string of image path here>)
+  selectedImage?: string | null; // require(<string of image path here>)
+}
+
+export default function ImageViewer({
+  placeholderImageSource,
+  selectedImage,
+}: ImageViewerProps) {
+  return (
+    <Image
+      source={selectedImage ? { uri: selectedImage } : placeholderImageSource}
+      style={styles.image}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
